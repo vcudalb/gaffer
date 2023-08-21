369b2770e4f40aedca5401e6cce46ed4ba9dfdc1
vcudalb <cudalb.vasile@gmail.com>
2023-08-21 23:45:13 +0300

    feat: base project structure and bark command

---
+++ src/gaffer.js
@@ -0,0 +1,30 @@
+﻿require('dotenv').config();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = createClient();
+
+client.once(Events.ClientReady, () => {
+    console.log(`Client: ${client.user.tag} ready to receive messages since:${new Date().toUTCString()}!`);
+});
+
+client.on(Events.MessageCreate, function (message) {
+    if (message.author.bot) return;
+
+    if (message.content === "!bark") {
+        message.reply(`Gaf gaf ${message.author.displayName} !`)
+            .catch(error => console.error(`Error replying to bark ${message.content}:`, error));
+    }
+});
+
+client.login(process.env.CLIENT_TOKEN).then(r => console.log(`Client: ${client.user.tag} successfully logged at:${new Date().toUTCString()}`));
+
+function createClient(){
+    return new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+            GatewayIntentBits.GuildMembers,
+        ]
+    });
+}
