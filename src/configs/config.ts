e1e24dd455525c6a9566fedbdc6707b27443bf68
vcudalb <cudalb.vasile@gmail.com>
2023-12-25 01:39:18 +0200

    feat: typescript migration v5

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import dotenv from "dotenv";
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 interface ENV {
     CLIENT_TOKEN: string | undefined;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import 'dotenv/config';
 import container from "./inversify.config";
 import {TYPES} from "./Types";
