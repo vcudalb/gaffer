aa33d4219652d96ea81e39006b5eebe4ed4de7f9
vcudalb <cudalb.vasile@gmail.com>
2023-12-29 00:46:29 +0200

    feat: typescript migration v8

--- src/commands/weather.ts
+++ src/commands/weather.ts
@@ -1,4 +1,4 @@
-import {CommandInteraction, SlashCommandBuilder} from 'discord.js';
+import {ChatInputCommandInteraction, CommandInteraction, SlashCommandBuilder} from 'discord.js';
 // @ts-ignore
 import { get } from 'axios';
 // @ts-ignore
@@ -9,46 +9,35 @@ const API_URLS = {
 };
 
 const slashCommand = new SlashCommandBuilder()
-    .setName("weather").setDescription("Shows remaining time until you will be destroyed and humiliated by Masya (23:00)");
+    .setName("weather")
+    .setDescription("Provides weather stats for a specific location")
+    .addStringOption(option =>
+        option.setName('location')
+            .setDescription('Location for which the weather will be displayed')
+            .setRequired(true)
+    );
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+
+    let embedsProvider = new EmbedsProvider();
+    const { commandName, options } = interaction;
+    let location = options.getString('location');
+
+    try {
+        const response = await get(`${API_URLS.current}?key=${process.env.WEATHER_API_KEY}&q=${location}`);
+        const status = response.status;
+
+        if (status === 200) {
+            const weatherData = response.data;
+            const embeds = embedsProvider.getWeatherEmbeds(weatherData);
+            await interaction.reply({embeds: [embeds]});
+        } else {
+            await interaction.reply('Failed to fetch weather data');
+        }
+    } catch (error) {
+        console.error(error);
+        await interaction.reply('An error occurred while fetching the weather data.');
+    }
+}
 
 export {slashCommand}
-export const weather = {
-    data: {
-        name: 'weather',
-        description: 'Provides weather stats for a specific location',
-        options: [
-            {
-                name: 'location',
-                description: 'The location for which you want to get the weather',
-                type: 'STRING',
-                required: true,
-            },
-        ],
-    },
-    async execute(interaction: CommandInteraction): Promise<void> {
-        
-        var embedsProvider = new EmbedsProvider();
-        const location = "";//interaction.options.get('location').value as string;
-
-        if (!location) {
-            await interaction.reply('`location` parameter is required. Example `/weather Chisinau`.');
-            return;
-        }
-
-        try {
-            const response = await get(`${API_URLS.current}?key=${process.env.WEATHER_API_KEY}&q=${location}`);
-            const status = response.status;
-
-            if (status === 200) {
-                const weatherData = response.data;
-                const embeds = embedsProvider.getWeatherEmbeds(weatherData);
-                await interaction.reply({ embeds: [embeds] });
-            } else {
-                await interaction.reply('Failed to fetch weather data');
-            }
-        } catch (error) {
-            console.error(error);
-            await interaction.reply('An error occurred while fetching the weather data.');
-        }
-    },
-};
