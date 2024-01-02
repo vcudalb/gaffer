00b87f907a5312ea49f47a58d309aec61dadb4fe
vcudalb <cudalb.vasile@gmail.com>
2024-01-03 00:03:28 +0200

    chore: small weather adjustments

--- src/slashCommands/weather.ts
+++ src/slashCommands/weather.ts
@@ -1,43 +1,40 @@
-import {SlashCommandBuilder} from "discord.js"
-import {SlashCommand} from "../../global";
-import get from "axios";
-import {EmbedsProvider} from "../core/providers/EmbedsProvider";
+import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommand } from '../../global';
+import axios from 'axios';
+import { EmbedsProvider } from '../core/providers/EmbedsProvider';
 
 const command: SlashCommand = {
     command: new SlashCommandBuilder()
-        .setName("weather")
-        .setDescription("Provides weather stats for a specific location")
-        .addStringOption(option => {
-            return option
-                .setName("location")
-                .setDescription("Location for which the weather will be displayed")
+        .setName('weather')
+        .setDescription('Provides weather stats for a specific location')
+        .addStringOption((option) =>
+            option
+                .setName('location')
+                .setDescription('Location for which the weather will be displayed')
                 .setRequired(true)
-        }) as SlashCommandBuilder,
+        ) as SlashCommandBuilder,
 
-    execute: async interaction => {
-        let embedsProvider = new EmbedsProvider();
-        const {commandName, options} = interaction;
-        let location = options.getString('location');
+    execute: async (interaction) => {
+        const embedsProvider = new EmbedsProvider();
+        const location = interaction.options.getString('location');
 
         try {
-            const response = await get(`${API_URLS.current}?key=${process.env.WEATHER_API_KEY}&q=${location}`);
-            const status = response.status;
+            const response = await axios.get(`${API_URLS.current}?key=${process.env.WEATHER_API_KEY}&q=${location}`);
+            const { status, data: weatherData } = response;
 
             if (status === 200) {
-                const weatherData = response.data;
                 const embeds = embedsProvider.getWeatherEmbeds(weatherData);
-                await interaction.reply({embeds: [embeds]});
-                await interaction.deleteReply();
+                await interaction.reply({ embeds: [embeds] });
             } else {
-                await interaction.reply('Failed to fetch weather data'.toString());
+                await interaction.reply('Failed to fetch weather data');
             }
         } catch (error) {
             console.error(error);
-            await interaction.reply('An error occurred while fetching the weather data.'.toString());
+            await interaction.reply('An error occurred while fetching the weather data.');
         }
     },
-    cooldown: 60
-}
+    cooldown: 2,
+};
 
 const API_URLS = {
     current: 'https://api.weatherapi.com/v1/current.json',
